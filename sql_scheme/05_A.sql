cfb17ab4e0aa87cc265ece8f4b3ae72a26a0b4a9
kurt-jadson <jadsonfrassetto@hotmail.com>
2014-06-16 20:31:38 -0300

    Update 05_A.sql

--- sql_scheme/05_A.sql
+++ sql_scheme/05_A.sql
@@ -2,3 +2,43 @@ INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTER
     VALUES (1, 'Qual a cor do cavalo branco de Napoleão?', 'Amarela', 'Preta', 'Branca', 'Azul', 'C', 10, 'GERAL', 60);
 INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
     VALUES (2, 'Qual dos seguintes protocolos é utilizado para transferência íntegra dos dados em conexões fim a fim?', 'UDP', 'TCP', 'POP', 'SMTP', 'B', 40, 'GERAL', 60);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (3, 'Qual é a capital da Alemanha?', 'Hamburgo', 'Berlim', 'Frankfurt', 'Moscou', 'B', 50, 'GERAL', 60);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (4, 'Qual desses pontos turísticos está no egito?', 'A estátua da liberdade', 'As grandes pirâmides', 'A torre eiffel', 'O cristo redentor', 'B', 40, 'GERAL', 60);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (5, 'Qual desses países é vizinho do Brasil?', 'Áustria', 'Argentina', 'Arábia Saudita', 'Bósnia', 'B', 10, 'GERAL', 30);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (6, 'Como se chamam as ruínas astecas no Peru?', 'Cachu Titchu', 'Bachu Bichu', 'Pickachu', 'Machu Picchu', 'D', 50, 'GERAL', 60);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (7, 'Qual o único monumento que pode ser visto do espaço?', 'O cristo redentor', 'O arco do triunfo', 'As muralhas da china', 'A ponte do tietê', 'C', 50, 'GERAL', 40);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (8, 'Onde nasceu o grande pintor Leonardo da Vinci?', 'Itália', 'França', 'Alemanha', 'Brasil', 'A', 90, 'GERAL', 60);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (9, 'Qual desses países não fala português?', 'Portugal', 'Brasil', 'Angola', 'Holanda', 'D', 30, 'GERAL', 60);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (10, 'Em que país está boa parte da cordilheira dos andes?', 'China', 'Paraguai', 'Chile', 'Venezuela', 'C', 70, 'GERAL', 60);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (11, 'Quem nasce na Bolívia é?', 'Bolsonaro', 'Boliviense', 'Bolívar', 'Boliviano', 'D', 80, 'GERAL', 60);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (12, 'Qual maior país do mundo?', 'China', 'Paraguaia', 'Estados Unidos', 'Rússia', 'D', 40, 'GERAL', 60);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (13, 'Há uma caixa que armazena metade da capacidade de uma maior. Se a maior armazena 20 kilos. Quanto as duas juntas armazenam?', '25 kilos', '12,5 kilos', '30 kilos', '40 kilos', 'C', 50, 'GERAL', 70);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (14, 'O leite está para o copo assim como a carta está para :', 'O selo', 'A caneta', 'O livro', 'O envelope', 'D', 40, 'GERAL', 60);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (15, 'Termo usado para designar os alimentos geneticamente modificados.', 'Genéticos', 'Transgênicos', 'Estéticos', 'Hidrogenados', 'B', 50, 'GERAL', 60);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (16, 'O castor é uma espécie de:', 'Morcego', 'Vegetal', 'Roedor', 'Ave sem penas', 'C', 20, 'GERAL', 40);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (17, 'Distúrbio psicológico onde o paciente tem pavor de ambientes abertos.', 'Hidrofobia', 'Aerofobia', 'Claustrofobia', 'Agorafobia', 'D', 90, 'GERAL', 60);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (18, 'Famoso artista do Renascimento Italiano que pintou o teto da Capela Sistina no Vaticano', 'Botticelli', 'Michalangelo', 'Leonardo Da Vinci', 'Rafael', 'B', 90, 'GERAL', 60);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (19, 'Qual é o principal alimento do urso panda?', 'Broto das palmeiras', 'Brotos de Bambu', 'Brotos de Samambaia', 'Capim silvestres', 'A', 60, 'GERAL', 60);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (20, 'Qual desses mamíferos não consegue pular?', 'Elefante', 'Golfinho', 'Foca', 'Preguiça', 'A', 40, 'GERAL', 60);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (21, 'Qual dessas cores não está relacionada a coleta seletiva?', 'Vermelho', 'Azul', 'Amarelo', 'Rosa', 'D', 20, 'GERAL', 40);
+INSERT INTO QUESTION (`ID`, `QUESTION`, `ALTERNATIVE_A`, `ALTERNATIVE_B`, `ALTERNATIVE_C`, `ALTERNATIVE_D`, `ANSWER`, `AMOUNT`, `THEME`, `TIMETOANSWER`)
+    VALUES (22, 'O que ocorre com a água quando congela?', 'Evapora', 'Diminui o volume', 'Aumenta o volume', 'Derrete', 'C', 50, 'GERAL', 60);
